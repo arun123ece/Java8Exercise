21575a64d602f0173cb0c27dc789536d6702c248
ubuntu <ubuntu@ip-172-31-34-12.ec2.internal>
2019-10-23 11:42:32 +0000

    Final Commit

--- src/main/java/com/learn/e05/datetime/WorkingDaysExercise.java
+++ src/main/java/com/learn/e05/datetime/WorkingDaysExercise.java
@@ -5,10 +5,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WorkingDaysExercise {
 
@@ -25,9 +24,9 @@ public class WorkingDaysExercise {
 		LocalDate today = LocalDate.now();
         LocalDate nextMonthFirstDay = today.with(TemporalAdjusters.firstDayOfNextMonth());
         LocalDate nextMonthLastDate = nextMonthFirstDay.plusMonths(1);
-        int noOfDays = (int) ChronoUnit.DAYS.between(nextMonthFirstDay, nextMonthLastDate);
+        //int noOfDays = (int) ChronoUnit.DAYS.between(nextMonthFirstDay, nextMonthLastDate);
         
-        List<String> dates = new ArrayList<String>(noOfDays);
+       /* List<String> dates = new ArrayList<String>(noOfDays);
         
         Set<DayOfWeek> weekend =  EnumSet.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY) ;
         while( nextMonthFirstDay.isBefore(nextMonthLastDate) ) {
@@ -36,7 +35,15 @@ public class WorkingDaysExercise {
             }
             nextMonthFirstDay = nextMonthFirstDay.plusDays(1);
         }
-        return dates;
+        return dates;*/
+        List<String> dates = Stream.iterate(nextMonthFirstDay, (d) -> d.plusDays(1))
+                .filter(i -> !(i.getDayOfWeek() == DayOfWeek.SATURDAY || i.getDayOfWeek() == DayOfWeek.SUNDAY))
+                .limit(ChronoUnit.DAYS.between(nextMonthFirstDay, nextMonthLastDate))
+                .filter(i -> i.isBefore(nextMonthLastDate))
+                .map(i -> i.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")).toString())
+               .collect(Collectors.toList());
+       
+       return dates;
 	}
 	public static void main(String[] args) {
 		
